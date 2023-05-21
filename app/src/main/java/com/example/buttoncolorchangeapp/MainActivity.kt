422c1cedd48fd6f0ceb44166a534331b7447f21b
coding meet <codingmeet26@gmail.com>
2023-05-21 20:42:35 +0530

    implement splash api

--- app/src/main/java/com/example/buttoncolorchangeapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncolorchangeapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.buttoncolorchangeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // here setKeepOnScreenCondition true so, activity redirect another activity
+        // and some api call here
+        // if setKeepOnScreenCondition false so, activity code not redirect another activity
+        splashScreen.setKeepOnScreenCondition { false }
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, SecondActivity::class.java))
+//            finish()
+//        }, 2000)
     }
 }
